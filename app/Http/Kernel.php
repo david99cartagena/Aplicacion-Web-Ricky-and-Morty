2c45005c37270ca415f7e977970d98662e9b5f54
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2025-07-15 00:26:03 -0500

    middlare para kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,5 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'isUserAuth' => \App\Http\Middleware\IsUserAuth::class,
+        'isAdmin' => \App\Http\Middleware\IsAdmin::class,
     ];
 }
