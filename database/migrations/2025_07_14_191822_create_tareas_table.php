a96eb0eba7fabe4d32be2ab8053b419833d50c0d
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2025-07-15 00:24:10 -0500

    migraciones

---
+++ database/migrations/2025_07_14_191822_create_tareas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTareasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('titulo');
+            $table->text('descripcion')->nullable();
+            $table->date('fecha_vencimiento');
+            $table->enum('estado', ['Pendiente', 'En progreso', 'Completada'])->default('Pendiente');
+            $table->integer('rick_morty_personaje_id')->nullable(); // solo el ID, sin consumir la API
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tareas');
+    }
+}
